01d75a3ba81e8b23b238f1d35a54951d59a2ee46
Rafael Diego Gomes de Oliveira <rafadiego.gomes@gmail.com>
2020-01-22 16:51:35 -0300

    Destructuring

--- app1/App.js
+++ app1/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+const estilo = {
+  HelloWorldApp: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  text: {color: '#fff', backgroundColor: 'red'},
+};
 export default class HelloWorldApp extends Component {
   render() {
-    const estilo = {
-      HelloWorldApp: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-      text: {color: '#fff', backgroundColor: 'red'}
-    };
-
+    const {HelloWorldApp, text} = estilo;
     return (
-      <View style={estilo.HelloWorldApp}>
-        <Text style={estilo.text}>Hello, React World!</Text>
+      <View style={HelloWorldApp}>
+        <Text style={text}>Hello, React World!</Text>
       </View>
     );
   }
