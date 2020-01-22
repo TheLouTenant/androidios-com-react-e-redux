5c08bdc60a841c6b2173e15d2b65840b918a7840
Rafael Diego Gomes de Oliveira <rafadiego.gomes@gmail.com>
2020-01-22 16:48:39 -0300

    Estilo em variável

--- app1/App.js
+++ app1/App.js
@@ -5,11 +5,12 @@ export default class HelloWorldApp extends Component {
   render() {
     const estilo = {
       HelloWorldApp: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+      text: {color: '#fff', backgroundColor: 'red'}
     };
 
     return (
       <View style={estilo.HelloWorldApp}>
-        <Text>Hello, React World!</Text>
+        <Text style={estilo.text}>Hello, React World!</Text>
       </View>
     );
   }
