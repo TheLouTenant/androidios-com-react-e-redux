c976bd55dc7858cf5bfb59f8f39cf3bc5b189fa9
Rafael Diego Gomes de Oliveira <rafadiego.gomes@gmail.com>
2020-01-22 16:45:41 -0300

    Estilo em variável

--- app1/App.js
+++ app1/App.js
@@ -3,8 +3,12 @@ import { Text, View } from 'react-native';
 
 export default class HelloWorldApp extends Component {
   render() {
+    const estilo = {
+      HelloWorldApp: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+    };
+
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={estilo.HelloWorldApp}>
         <Text>Hello, React World!</Text>
       </View>
     );
