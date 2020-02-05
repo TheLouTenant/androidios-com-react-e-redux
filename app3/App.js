8dfcc3babf49de5b427b9a47c943956d39878356
Rafael Diego Gomes de Oliveira <rafadiego.gomes@gmail.com>
2020-02-05 16:28:40 -0300

    Primeiro Componente

--- app3/App.js
+++ app3/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const estilo = {
-  HelloWorldApp: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-  text: {color: '#fff', backgroundColor: 'red'},
-};
-export default class HelloWorldApp extends Component {
+class MeuComponente extends Component {
   render() {
-    const {HelloWorldApp, text} = estilo;
     return (
-      <View style={HelloWorldApp}>
-        <Text style={text}>Hello, React World!</Text>
+      <View>
+        <Text>Primeiro Parametro</Text>
+        <Text>Segundo Parametro</Text>
       </View>
     );
+  }
+}
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return <MeuComponente></MeuComponente>;
   }
 }
